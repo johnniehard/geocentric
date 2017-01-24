e9f94dd4a6b320cf65551596bda3740f0bd54b3a
johnnie <johnnie.hard@gmail.com>
2017-01-24 17:00:10 +0100

    move jsonloader to its own file

---
+++ jsonloader.js
@@ -0,0 +1,14 @@
+//from: http://stackoverflow.com/questions/14388452/how-do-i-load-a-json-object-from-a-file-with-ajax
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
