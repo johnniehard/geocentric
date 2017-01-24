ed90d612415f42085eb1cb79b03095d83df72346
johnnie <johnnie.hard@gmail.com>
2017-01-24 17:23:09 +0100

    adding some key functions missing from p5js

--- geocentric.js
+++ geocentric.js
@@ -13,15 +13,15 @@ function Ellipsoid(a = 6378137, f = 298.257223563){ //defaults to wgs84
 }
 
 Ellipsoid.prototype.getX = function(lat, lon, h){
-	return (this.getNprime(lat) + h) * cos(radians(lat)) * cos(radians(lon));
+	return (this.getNprime(lat) + h) * Math.cos(radians(lat)) * Math.cos(radians(lon));
 }
 
 Ellipsoid.prototype.getY = function(lat, lon, h){
-	return (this.getNprime(lat) + h) * cos(radians(lat)) * sin(radians(lon));
+	return (this.getNprime(lat) + h) * Math.cos(radians(lat)) * Math.sin(radians(lon));
 }
 
 Ellipsoid.prototype.getZ = function(lat, h){
-	return (this.getNprime(lat) * (1 - this.eSq) + h) * sin(radians(lat));
+	return (this.getNprime(lat) * (1 - this.eSq) + h) * Math.sin(radians(lat));
 }
 
 Ellipsoid.prototype.getGeocentric = function(lat, lon, h){
@@ -29,7 +29,7 @@ Ellipsoid.prototype.getGeocentric = function(lat, lon, h){
 }
 
 Ellipsoid.prototype.getNprime = function(lat){
-	return this.a / Math.sqrt(1 - this.eSq * Math.pow(sin(radians(lat)), 2))
+	return this.a / Math.sqrt(1 - this.eSq * Math.pow(Math.sin(radians(lat)), 2))
 }
 
 Ellipsoid.prototype.geteSquared = function(f){
@@ -42,10 +42,22 @@ Ellipsoid.prototype.getLatLon = function(geocentric){
 	var theta = degrees(atan((geocentric[2]) / (p * Math.sqrt(1 - this.eSq))))
 	// prep for lat
 	var inner_division = (this.a * this.eSq) / (Math.sqrt(1 - this.eSq))
-	var above_division = geocentric[2] + inner_division * (sin(radians(theta)) ** 3)
+	var above_division = geocentric[2] + inner_division * (Math.sin(radians(theta)) ** 3)
 	// below division
-	var below_division = p - this.a * this.eSq * (cos(radians(theta)) ** 3)
+	var below_division = p - this.a * this.eSq * (Math.cos(radians(theta)) ** 3)
 	var lat = degrees(atan(above_division / below_division))
-	var h = ((p) / (cos(radians(lat)))) - this.getNprime(lat)
+	var h = ((p) / (Math.cos(radians(lat)))) - this.getNprime(lat)
 	return [lat, lon, h]
 }
+
+function radians(degrees){
+	return degrees * Math.PI / 180;
+}
+
+function degrees(radians){
+	return radians * 180 / Math.PI;
+}
+
+function map(n, start1, stop1, start2, stop2) {
+  return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+};
